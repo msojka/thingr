414c6cefc0cb049cb10e96308f86c0ebb2668c21
Martin Sojka <sojka.martin@gmail.com>
2015-11-22 23:15:18 +0100

    Add edit, update controller logic and tests.

--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -11,4 +11,16 @@ class ThingsController < ApplicationController
     render action: :new
   end
   
+  def edit
+    form Thing::Update
+    render action: :new
+  end
+  
+  def update
+    run Thing::Update do |op|
+      return redirect_to op.model
+    end
+    render action: :new
+  end
+  
 end
--- test/controllers/things_controller_test.rb
+++ test/controllers/things_controller_test.rb
@@ -1,8 +1,17 @@
-describe ThingsController do
-  
+require 'test_helper'
 
-  describe "create" do
-    
+describe ThingsController do
+  let (:thing) { Thing::Create.(thing: {name: "Rails"}).model }
+  
+  describe "new" do  
+    it do
+      get :new
+      assert_select "form #thing_name"
+      assert_select "form #thing_name.readonly", false
+    end
+  end
+  
+  describe "create" do  
     it do #valid
       post :create, {thing: {name: "Bad Religion"}}
       assert_redirected_to thing_path(Thing.last)
@@ -12,8 +21,12 @@ describe ThingsController do
       post :create, {thing: {name: ""}}
       assert_select ".error"
     end
-  
   end
-
-
+  
+  describe "edit" do  
+    it do
+      get :edit, id: thing.id
+      assert_select "form #thing_name.readonly[value='Rails']"
+    end
+  end
 end
