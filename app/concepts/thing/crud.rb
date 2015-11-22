6265cdc07f5750ac791a6a9141fa635a0d3eb034
Martin Sojka <sojka.martin@gmail.com>
2015-11-22 22:11:11 +0100

    Add Contract to Thing operation that instantiates Reform under the hood. Add Model module for simplifying CRUD.

--- app/concepts/thing/crud.rb
+++ app/concepts/thing/crud.rb
@@ -1,9 +1,25 @@
 class Thing < ActiveRecord::Base
   
+  
   class Create < Trailblazer::Operation
-    def process(params)
-      @model = Thing.create(params[:thing])
+    include Model
+    model Thing, :create
+    
+    contract do
+      property :name
+      property :description
+  
+      validates :name, presence: true
+      validates :description, length: {in: 4..160}, allow_blank: true
     end
+  
+    def process(params)
+      validate(params[:thing]) do |f|
+        f.save
+      end
+    end
+  
   end
+  
   
 end
--- app/controllers/things_controller.rb
+++ app/controllers/things_controller.rb
@@ -1,2 +1,10 @@
 class ThingsController < ApplicationController
+  
+  def create
+    run Thing::Create do |op|
+      return redirect_to op.model
+    end
+    render action: :new
+  end
+  
 end
