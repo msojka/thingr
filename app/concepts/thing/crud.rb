0adeaafd38d55015328d00c917fb51b6bc7326d7
Martin Sojka <sojka.martin@gmail.com>
2015-11-22 19:49:14 +0100

    Add Thing Create operation with a  passing test.

---
+++ app/concepts/thing/crud.rb
@@ -0,0 +1,9 @@
+class Thing < ActiveRecord::Base
+  
+  class Create < Trailblazer::Operation
+    def process(params)
+      @model = Thing.create(params[:thing])
+    end
+  end
+  
+end
---
+++ test/concepts/thing/crud_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class ThingCrudTest < MiniTest::Spec
+  
+  describe "Create" do
+    it "perists valid" do
+      thing = Thing::Create.(
+        thing: {
+          name: 'Rails',
+          description: 'Kickass web dev'
+        }
+      ).model
+      
+      thing.persisted?.must_equal true
+      thing.name.must_equal 'Rails'
+      thing.description.must_equal 'Kickass web dev'
+    end
+  end
+  
+end
