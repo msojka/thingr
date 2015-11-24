d50afc21d1e1ed99a982fa6f63907f4348dda40f
Martin Sojka <sojka.martin@gmail.com>
2015-11-24 16:52:08 +0100

    Add Thing Grid cell to wrap things grid. Update test.

--- app/concepts/thing/cell.rb
+++ app/concepts/thing/cell.rb
@@ -23,4 +23,11 @@ private
     classes
   end
   
+  class Grid < Cell::Concept
+    def show
+      things = Thing.latest
+      concept("thing/cell", collection: things, last: things.last)
+    end
+  end
+  
 end
--- test/concepts/thing/cell_test.rb
+++ test/concepts/thing/cell_test.rb
@@ -3,16 +3,15 @@ require 'test_helper'
 class ThingCellTest < Cell::TestCase
   controller ThingsController
   
-  let(:rails) { Thing::Create.(thing: { name: 'Rails' }).model }
-  let(:trailblazer) { Thing::Create.(thing: { name: 'Trailblazer' }).model }
-  
-  subject { concept("thing/cell", collection: [trailblazer, rails], last: rails) }
-  
   it do
-    subject.must_have_selector ".columns .header a", text: "Rails"
-    subject.wont_have_selector ".columns.end .header a", text: "Trailblazer"
-    subject.must_have_selector ".columns.end .header a", text: "Rails"
+    Thing::Create.(thing: { name: 'Rails' })
+    Thing::Create.(thing: { name: 'Trailblazer' })
+    
+    html = concept("thing/cell/grid").()
+    
+    html.must_have_selector ".columns .header a", text: "Rails"
+    html.wont_have_selector ".columns.end .header a", text: "Trailblazer"
+    html.must_have_selector ".columns.end .header a", text: "Rails"
   end
-  
 end
   
