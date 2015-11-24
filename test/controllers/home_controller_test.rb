268054f8d4e0de54e9ca59027aa2d3b469fa5611
Martin Sojka <sojka.martin@gmail.com>
2015-11-24 16:59:32 +0100

    Make home controller integration test just a smoke test.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,15 +1,10 @@
-# require 'test_helper'
-# 
-# class HomeIntegrationTest < Capybara::Rails::TestCase
-#   it do
-#     Thing.delete_all
-# 
-#     Thing::Create.(thing: {name: "Trailblazer"})
-#     Thing::Create.(thing: {name: "Rails"})
-# 
-#     visit "/"
-# 
-#     page.must_have_css ".columns .header a", text: "Rails" # TODO: test not-end.
-#     page.must_have_css ".columns.end .header a", text: "Trailblazer"
-#   end
-# end
+require 'test_helper'
+
+class HomeIntegrationTest < ActionDispatch::IntegrationTest
+  it do
+    Thing::Create.(thing: {name: "Rails"})
+    # "Smoke" test .. just make sure that Thing grid cell is included.
+    get "/"
+    assert_select ".header a", text: "Rails"
+  end
+end
