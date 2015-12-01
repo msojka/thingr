2dc38e5ffc9c1444b272204bf8c2d23c3b68972a
Martin Sojka <sojka.martin@gmail.com>
2015-12-01 16:10:21 +0100

    Move thing form back to contract.

--- app/concepts/thing/crud.rb
+++ app/concepts/thing/crud.rb
@@ -4,9 +4,6 @@ class Thing < ActiveRecord::Base
     include Model
     model Thing, :create
     
-    require_dependency "thing/form"
-    self.contract_class = Form
-    
     include Dispatch
     callback do
       collection :users do
@@ -14,6 +11,39 @@ class Thing < ActiveRecord::Base
         on_add :reset_authorship!
       end
       on_change :expire_cache!
+    end
+    
+    contract do
+      property :name
+      property :description
+
+      validates :name, presence: true
+      validates :description, length: {in: 4..160}, allow_blank: true
+      
+      collection :users, 
+        prepopulator: :prepopulate_users!,
+        populate_if_empty: :populate_users!,
+        skip_if: :all_blank do
+          property :email
+          validates :email, presence: true, email: true
+          validate :authorships_limit_reached?
+          
+      private
+        def authorships_limit_reached?
+          return if model.authorships.find_all { |au| au.confirmed == 0 }.size < 5
+          errors.add("user", "This user has too many unconfirmed authorships.")
+        end
+      end
+      validates :users, length: { maximum: 3 }
+      
+    private
+      def prepopulate_users!(options)
+        (3 - users.size).times{ users << User.new }
+      end
+      
+      def populate_users!(params, options)
+        User.find_by_email(params[:email]) or User.new
+      end
     end
   
     def process(params)
